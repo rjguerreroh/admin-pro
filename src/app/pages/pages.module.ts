ce6215d000e597936037abb162c94ce172bded9e
Roberto Guerrero <rjguerreroh@gmail.com>
2020-11-01 11:51:50 -0500

    Cambios video 76

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -11,6 +11,8 @@ import { Grafica1Component } from './grafica1/grafica1.component';
 import { PagesComponent } from './pages.component';
 import { SharedModule } from '../shared/shared.module';
 import { AccountSettingsComponent } from './account-settings/account-settings.component';
+import { PromesasComponent } from './promesas/promesas.component';
+import { RxjsComponent } from './rxjs/rxjs.component';
 
 @NgModule({
   declarations: [
@@ -19,6 +21,8 @@ import { AccountSettingsComponent } from './account-settings/account-settings.co
     Grafica1Component,
     PagesComponent,
     AccountSettingsComponent,
+    PromesasComponent,
+    RxjsComponent,
   ],
   exports: [
     DashboardComponent,
--- src/app/services/sidebar.service.ts
+++ src/app/services/sidebar.service.ts
@@ -14,6 +14,8 @@ export class SidebarService {
         { titulo: 'Main', url: '/'},
         { titulo: 'ProgressBar', url: 'progress'},
         { titulo: 'Gráficas', url: 'grafica1'},
+        { titulo: 'Promesas', url: 'promesas'},
+        { titulo: 'rxjs', url: 'rxjs'},
       ]
     }
   ];
